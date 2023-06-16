a856399b4594da94eb522bd966309fedae5d768e
farid <makflay85zet@gmail.com>
2023-06-17 00:31:44 +0400

    added lesson04_DZ added description

--- src/main/java/lesson04_DZ/Main01_DZ.java
+++ src/main/java/lesson04_DZ/Main01_DZ.java
@@ -1,5 +1,27 @@
 package lesson04_DZ;
 
+//Урок 4. Хранение и обработка данных ч1: приоритетные коллекции
+//        Реализовать консольное приложение, которое:
+//        1. Принимает от пользователя и “запоминает” строки.
+//        2. Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
+//        3. Если введено revert, удаляет предыдущую введенную строку из памяти.
+//        4. Если введено exit, то программа завершается
+//
+//        > - ввод в консоль (stdin), < - вывод на консоль (stdout)
+//        > java
+//        > python
+//        > c#
+//        > print
+//        < [c#, python, java]
+//        > revert
+//        > print
+//        < [python, java]
+//        > revert
+//        > revert
+//        > print
+//        < []
+//        > revert -> throw new NoSuchElementException
+
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Scanner;
