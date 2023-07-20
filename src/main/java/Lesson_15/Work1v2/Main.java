1c7d78e1fbdab11e411c7283605a37d8dc7e5f82
farid <makflay85zet@gmail.com>
2023-07-20 06:42:58 +0400

    added lesson15_DZv3

--- src/main/java/Lesson_15/Work1v2/Main.java
+++ src/main/java/Lesson_15/Work1v2/Main.java
@@ -1,5 +1,17 @@
 package Lesson_15.Work1v2;
+/*
+Доработка, предыдущего кода:
+1.Figure на два интерфейса: RectangleFigure и SquareFigure.
+  Каждый интерфейс предоставляет свои уникальные методы,
+  связанные с установкой параметров фигур,
+  что соблюдает принцип разделения интерфейсов.
+2.В классах Rectangle и Square добавили конструкторы
+для инициализации высоты и ширины (для Rectangle) или только высоты (для Square).
+Таким образом, объекты фигуры создаются в инициализированном состоянии,
+а не через методы setHeight() и setWidth().
+3....
 
+*/
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(3,5);
